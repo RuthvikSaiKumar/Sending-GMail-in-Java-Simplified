cec90a87fac408ca180651d37e5a5e85075eb2a8
RuthvikSaiKumar <86515993+RuthvikSaiKumar@users.noreply.github.com>
2021-07-02 20:28:49 +0530

    Create MailSender.java

---
+++ MailSender.java
@@ -0,0 +1,18 @@
+public class MailSender {
+    public static void main(String[] args) {
+        GMail mail=new GMail("senderaddress@gmail.com","senderpassword","senderaddress@gmail.com","TO","recieveraddress1@gmail.com", "recieveraddress2@gmail.com");
+        /*
+         First argument is the senders E-mail for authentication
+         Second argument is the senders E-mail password for authentication
+         Third argument is the senders E-mail to send the mail
+         Fourth argument is the type of recieving Message TO | CC | BCC  [ it is not case-sensitive ]
+         
+         From fifth argument it is the recievers' E-mail addresses [ it uses var-args for accepting multiple addresses [ please search for var-arg in Java if you don't know what it is ] ]
+            You can either put a String array of the recievers' address or you can just continue to enter as arguments
+        */
+        mail.setSubject("multi mail test"); // to set the subject
+        mail.addText("test send"); // to add the body text
+        mail.addAttachment("/mailattachmenttest.txt"); // to add attachment
+        System.out.println(mail.send()); // to display true or false if the mail is sent successfully or not respectively
+    }
+}
